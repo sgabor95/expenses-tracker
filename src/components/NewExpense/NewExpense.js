9d95ba3b79a97054811ad4e272ecae8de6394747
sgabor95 <mr35stefan@gmail.com>
2023-02-13 15:58:55 +0200

    Show button or form alternatively

--- src/components/NewExpense/NewExpense.js
+++ src/components/NewExpense/NewExpense.js
@@ -1,17 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import ExpenseForm from "./ExpenseForm";
 
 import "./NewExpense.css";
 
 const NewExpense = (props) => {
+  const [showForm, setShowForm] = useState(false);
+
   const saveExpenseDataHandler = (expenseDataInput) => {
     const expenseData = { ...expenseDataInput, id: Math.random().toString() };
     props.onAddNewExpense(expenseData);
   };
 
+  const onClickAddNewExpenseHandler = () => setShowForm(true);
+  const onClickCancelNewExpenseHandler = () => setShowForm(false);
+
   return (
     <div className="new-expense">
-      <ExpenseForm onSaveExpenseData={saveExpenseDataHandler} />
+      {!showForm && (
+        <button onClick={onClickAddNewExpenseHandler}>Add new expense</button>
+      )}
+      {showForm && (
+        <ExpenseForm
+          onSaveExpenseData={saveExpenseDataHandler}
+          onCancel={onClickCancelNewExpenseHandler}
+        />
+      )}
     </div>
   );
 };
